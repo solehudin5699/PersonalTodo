78805621accf935304f2f0254b3f5af21031a6d7
solehudin5699 <solehudinamqi@yahoo.co.id>
2021-05-08 18:46:15 +0700

    remove unused comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,13 @@
-// import { applyMiddleware, createStore } from "redux";
-// import { createLogger } from "redux-logger";
-// import rpm from "redux-promise-middleware";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage/session";
-// import indexReducer from "./reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, indexReducer);
-
-// //MIDDLWARE
-// const logger = createLogger();
-// const enhancers = applyMiddleware(rpm, logger);
-
-// let store = createStore(persistedReducer, enhancers);
-// let persistor = persistStore(store);
-
-// export { store, persistor };
-
-import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-import indexReducer from "./reducers/";
+import indexReducer from './reducers/';
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
 };
 const persistedReducer = persistReducer(persistConfig, indexReducer);
